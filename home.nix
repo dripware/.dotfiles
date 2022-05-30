21355329061eccd0d434927110046b89bc887267
dripware <itsdripware@gmail.com>
2022-05-30 17:33:54 +0430

    urxvt

--- home.nix
+++ home.nix
@@ -1,7 +1,4 @@
 { pkgs, ... }: {
-  imports = [
-    ./config/urxvt.nix
-  ];
   programs.home-manager.enable = true;
   home.packages = with pkgs; [
     neofetch
@@ -18,11 +15,23 @@
     wezterm
     tree
     hyperfine
+    (perlPackages.buildPerlModule rec {
+      pname = "Text-Bidi";
+      version = "2.16";
+      src = fetchurl {
+        url = "mirror://cpan/authors/id/K/KA/KAMENSKY/${pname}-${version}.tar.gz";
+	sha256 = "0000000000000000000000000000000000000000000000000000";
+      };
+    })
   ];
 
   home.file = {
-    ".local/bin" = {
+    ".local/bin" = { # custom scripts
       source = ./bin;
     };
+    ".urxvt/ext" = { # urxvt extensions
+      source = ./config/urxvt/ext;
+    };
   };
+  programs.urxvt = import ./config/urxvt;
 }
