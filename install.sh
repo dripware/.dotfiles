64ecf5ff2b0d3ebbf9335ae8e5b4429ba2e10406
dripware <itsdripware@gmail.com>
2022-06-08 10:19:31 +0000

    finish fixing install.sh (hopefully)

--- install.sh
+++ install.sh
@@ -159,9 +159,14 @@ fetch_dotfiles(){
 	__print "copying dotfiles to newly installed nixos..."
 	cp $HERE /mnt/home/$USERNAME/tmp_repo -r
 	nixos-enter --root /mnt -c "sudo -Hu $USERNAME git clone /home/$USERNAME/tmp_repo /home/$USERNAME/.dotfiles"
+<<<<<<< HEAD
 	nixos-enter --root "rm -rf /home/$USERNAME/tmp_repo"
+=======
+>>>>>>> 02bcc3d (finish fixing install.sh (hopefully))
 	rm -rf /mnt/home/$USERNAME/.dotfiles/.git/hooks
-	nixos-enter --root /mnt -c "ln /home/$USERNAME/.dotfiles/.githooks /home/$USERNAME/.dotfiles/.git/hooks -s"
+	nixos-enter --root /mnt -c "chown $USERNAME /home/$USERNAME/tmp_repo/system_local -R"
+	nixos-enter --root /mnt -c "sudo -Hu $USERNAME cp /home/$USERNAME/tmp_repo/system_local /home/$USERNAME/.dotfiles/system_local -r"
+	nixos-enter --root /mnt -c "sudo -Hu $USERNAME ln /home/$USERNAME/.dotfiles/.githooks /home/$USERNAME/.dotfiles/.git/hooks -s"
 }
 install_homemanager(){
 	__print "installing home-manager..."
@@ -170,15 +175,15 @@ install_homemanager(){
 	nixos-enter --root /mnt -c "rm -rf /home/$USERNAME/result"
 }
 
-check_root
+# check_root
 ask_for_inputs
-clean_before_install
-partition_disk
-format_partitions
-mount_partitions
-generate_system_system_local
-git_add_system_local
-update_flake
-install_nixos
+# clean_before_install
+# partition_disk
+# format_partitions
+# mount_partitions
+# generate_system_system_local
+# git_add_system_local
+# update_flake
+# install_nixos
 fetch_dotfiles
 install_homemanager
