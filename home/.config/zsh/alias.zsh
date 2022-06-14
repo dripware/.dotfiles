35b5ab6eea0a85328833eb8212adb201aef1ab52
dripware <itsdripware@gmail.com>
2022-06-14 22:08:00 +0430

    urxvt in ~/.local/bin to launch urxvtc if possible

--- home/.config/zsh/alias.zsh
+++ home/.config/zsh/alias.zsh
@@ -1,6 +1,2 @@
 #!/usr/bin/env zsh
-alias wget='wget --hist-file="${XDG_CACHE_HOME:-~/.cache}/wget-hsts"'
-if [[ ! -z $(ps cax | grep urxvtd) ]]; then
-	alias urxvt="$(which urxvtc)"
-	alias urxvtc="$(which urxvt)"
-fi
+alias wget='wget --hist-file="${XDG_CACHE_HOME:-~/.cache}/wget-hsts"' # move history file from $HOME
