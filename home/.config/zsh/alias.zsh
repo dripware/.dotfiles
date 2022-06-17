7a0c26fb51506dda75fa0ae8796caa7da9f7c462
dripware <itsdripware@gmail.com>
2022-06-17 23:06:46 +0430

    finish zert

--- home/.config/zsh/alias.zsh
+++ home/.config/zsh/alias.zsh
@@ -1,2 +1,29 @@
 #!/usr/bin/env zsh
-alias wget='wget --hist-file="${XDG_CACHE_HOME:-~/.cache}/wget-hsts"' # move history file from $HOME
+
+# move wget history file to better place for cleaner $HOME
+alias wget='wget --hist-file="${XDG_CACHE_HOME:-~/.cache}/wget-hsts"' 
+
+# make ls use colors and hide group names in ls -al
+alias ls='ls --color=tty -G' 
+
+# commonly used aliases for ls
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
+
+# make diff use colors
+alias diff='diff --color'
+
+# make grep use colors and ignore some special folders
+GREP_OPTIONS="--color=auto --exclude-dir={.git,CVS,.hg,.bzr,.svn,.idea,.tox}"
+alias grep="grep $GREP_OPTIONS"
+alias egrep="egrep $GREP_OPTIONS"
+alias fgrep="fgrep $FREP_OPTIONS"
+unset GREP_OPTIONS
+
+# make file navigation easier when trying to reach parent directories
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
--- home/.config/zsh/rc.zsh
+++ home/.config/zsh/rc.zsh
@@ -1,5 +1,40 @@
 #!/usr/bin/env zsh
-setopt no_global_rcs
 source "$ZDOTDIR/alias.zsh"
-autoload -Uz $ZDOTDIR/zert/* # lazy load zert
-export ZERTDIR=$HOME/.local/share/zsh/zert
+
+# set colors for ls command
+[[ -z "$LS_COLORS" ]] && (( $+commands[dircolors] )) && eval "$(dircolors -b)"
+# use ls colors for zsh file completions aswell
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+
+#####################
+# PLUGINS 
+#####################
+# initialize zert (zsh diy package manager)
+autoload -Uz "$ZDOTDIR/zert/zert"
+zert init
+
+# OH MY ZSH 
+#####################
+# all plugins and libraries from oh my zsh framework should be defined
+# here. make sure oh my zsh utilization is done after loading normal plugins
+# and utilizing prezto
+zert utilize ohmyzsh
+
+# cross-platform clipboard integration (clipcopy and clippaste commands)
+# clipcopy <file> # copies file content to clipboard
+# <command> | clipcopy # copies stdout to clipboard
+# clippaste > <file> # paste clipboard to file
+# clippaste | <command> # paste clipboard to stdin
+zert load @ohmyzsh:lib:clipboard
+
+
+#####################
+# ZSH OPTIONS 
+#####################
+# automatically add directories to "directory stack" after each cd
+# required for 'cd -' and 'cd -1','cd -2' etc.
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushd_silent
+setopt no_global_rcs
+setopt ksh_glob
