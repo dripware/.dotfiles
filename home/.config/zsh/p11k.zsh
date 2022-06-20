f2743cf1c8636acf89074134c22002b1c289464f
dripware <itsdripware@gmail.com>
2022-06-20 16:57:37 +0430

    fix transient prompt

--- home/.config/zsh/p11k.zsh
+++ home/.config/zsh/p11k.zsh
@@ -6,13 +6,17 @@
 # the prompt will change to become only the current directory
 # and the prompt_char
 function p10k-on-post-prompt() { # ran after pressing enter
-	MAIN_PROMPT=$POWERLEVEL9K_LEFT_PROMPT_ELEMENTS # save full prompt configs
+	MAIN_LEFT_PROMPT=$POWERLEVEL9K_LEFT_PROMPT_ELEMENTS # save full left prompt configs
+	MAIN_RIGHT_PROMPT=$POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS # save full right prompt configs
+
 	p10k display '1/*/*'=hide # hide everything
-	p10k display '1/left/(dir|prompt_char)'=show # print mini prompt
+	p10k display '1/left/(dir|prompt_char)'=show # print mini prompt left
+	p10k display '1/right/()'=show #print mini prompt right
 }
 function p10k-on-pre-prompt()  { # ran before new prompt prints
-	[[ ! -z "$MAIN_PROMPT" ]] && {
+	[[ ! -z "$MAIN_LEFT_PROMPT" ]] && {
 	p10k display '1/*/*'=hide # hide everything
-	p10k display "1/left/(${MAIN_PROMPT// /|})"=show #print full prompt
+	p10k display "1/left/(${MAIN_LEFT_PROMPT// /|})"=show #print full left prompt
+	p10k display "1/right/(${MAIN_RIGHT_PROMPT// /|})"=show #print full right prompt
 	}
 }
