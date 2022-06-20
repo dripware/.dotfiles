2177929d550df48f2b6ce81ee89d921a2efad962
dripware <itsdripware@gmail.com>
2022-06-20 21:55:42 +0430

    fix new line between prompts in powerlevel10k

--- home/.config/zsh/p11k.zsh
+++ home/.config/zsh/p11k.zsh
@@ -6,17 +6,32 @@
 # the prompt will change to become only the current directory
 # and the prompt_char
 function p10k-on-post-prompt() { # ran after pressing enter
-	MAIN_LEFT_PROMPT=$POWERLEVEL9K_LEFT_PROMPT_ELEMENTS # save full left prompt configs
-	MAIN_RIGHT_PROMPT=$POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS # save full right prompt configs
-
-	p10k display '1/*/*'=hide # hide everything
-	p10k display '1/left/(dir|prompt_char)'=show # print mini prompt left
-	p10k display '1/right/()'=show #print mini prompt right
+  # left full prompt configs
+  POWERLEVEL11K_MAIN_LEFT_PROMPT=$POWERLEVEL9K_LEFT_PROMPT_ELEMENTS 
+  # right full prompt configs
+  POWERLEVEL11K_MAIN_RIGHT_PROMPT=$POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS 
+  # left mini prompt configs
+  POWERLEVEL11K_MINI_LEFT_PROMPT="dir|prompt_char"
+  # right mini prompt configs
+  POWERLEVEL11K_MINI_RIGHT_PROMPT=""
+  # keep last executed command
+  POWERLEVEL11K_LAST_COMMAND="$BUFFER"
+  
+  # hide everything
+  p10k display '1/*/*'=hide 
+  # print left mini prompt
+  p10k display "1/left/($POWERLEVEL11K_MINI_LEFT_PROMPT)"=show 
+  #print mini prompt right
+  p10k display "1/right/($POWERLEVEL11K_MINI_RIGHT_PROMPT)"=show 
 }
 function p10k-on-pre-prompt()  { # ran before new prompt prints
-	[[ ! -z "$MAIN_LEFT_PROMPT" ]] && {
-	p10k display '1/*/*'=hide # hide everything
-	p10k display "1/left/(${MAIN_LEFT_PROMPT// /|})"=show #print full left prompt
-	p10k display "1/right/(${MAIN_RIGHT_PROMPT// /|})"=show #print full right prompt
-	}
+  [[ ! -z "$POWERLEVEL11K_LAST_COMMAND" ]] && [[ "clear" != "$POWERLEVEL11K_LAST_COMMAND" ]] && echo
+  [[ ! -z "$POWERLEVEL11K_MAIN_LEFT_PROMPT" ]] && {
+    # hide everything
+    p10k display '1/*/*'=hide 
+    #print full left prompt
+    p10k display "1/left/(${POWERLEVEL11K_MAIN_LEFT_PROMPT// /|})"=show 
+    #print full right prompt
+    p10k display "1/right/(${POWERLEVEL11K_MAIN_RIGHT_PROMPT// /|})"=show 
+  }
 }
